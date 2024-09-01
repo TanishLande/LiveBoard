4239d32d0134a8b462e4f04e9f453ea654ad85ca
Tanish Lande <tanishlande10@gmail.com>
2024-09-01 19:00:56 +0530

    side avatar

--- app/board/[id]/_components/participants.tsx
+++ app/board/[id]/_components/participants.tsx
@@ -1,12 +1,56 @@
+"use client"
+
 import { Skeleton } from "@/components/ui/skeleton"
+import { useOthers, useSelf } from "@/liveblocks.config"
+import { UserAvatar } from "./user-avatar"
+import { User } from "lucide-react";
+import { connectionIdtoColors } from "@/lib/utils";
+import { connect } from "http2";
+
+const MAX_OTHER_USER = 3;
 
 export const Participants = () =>{
+    const user = useOthers();
+    const currentuser = useSelf();
+    const hasMoreUser =  user.length > MAX_OTHER_USER;
+
     return(
         <div
         className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex ite-center shadow-md
         "
         >
-            List of user 
+            <div
+            className="flex gap-x-2"
+            >
+                {currentuser && (
+                    <UserAvatar
+                        borderColor="#0000CD"
+                        src={currentuser.info?.picture}
+                        name={`${currentuser.info?.name}(You)`}
+                        fallback={currentuser.info?.name?.[0]}
+                    />
+                )}
+
+                {user.slice(0,MAX_OTHER_USER).map(({connectionId, info})=>{
+                    return(
+                        <UserAvatar
+                            borderColor={connectionIdtoColors(connectionId)}
+                            key={connectionId}
+                            src={info?.picture}
+                            name={info?.name}
+                            fallback={info?.name?.[0] || "T"}
+                        />
+                    )
+                })}
+
+                {hasMoreUser && (
+                    <UserAvatar 
+                        name={`${user.length - MAX_OTHER_USER } more`}
+                        fallback={`+${user.length - MAX_OTHER_USER }`}
+                    />
+                )}
+
+            </div>
         </div>
     )
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,24 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+const COLORS= [
+  "#FF5733", // Red-Orange
+  "#33FF57", // Lime Green
+  "#3357FF", // Blue
+  "#F5F5F5", // Light Gray
+  "#FFC300", // Gold
+  "#FF33A1", // Pink
+  "#33FFF3", // Turquoise
+  "#E8E8E8", // Light Gray
+  "#7D3F00", // Brown
+  "#FF6347"  // Tomato
+];
+
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function connectionIdtoColors(connectionId: number): string{
+  return COLORS[connectionId % COLORS.length]
 }
